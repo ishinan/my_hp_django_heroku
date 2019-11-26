d3fef27b57cfe39da8ff2299d924a12b72aace82
Masaaki Yana <nativeyana@gmail.com>
2019-11-26 10:28:19 -0800

    api_github/views.py, github.html: extends base.html

--- api_github/views.py
+++ api_github/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 import requests
+from home.views import _get_current_year, _read_md_file
 
 # Create your views here.
 def get_repo(request):
+    copyright_year = _get_current_year()
     response = requests.get('https://api.github.com/users/ishinan/repos')
     repos = response.json()
     context = {
         'github_repos': repos,
+        'copyright_year': copyright_year,
     }
     return render(request, 'api_github/github.html', context) 
