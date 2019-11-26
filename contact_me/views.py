4225e43583e197716c7dde34bf1b3624ae93fa8b
Masaaki Yana <nativeyana@gmail.com>
2019-11-26 10:20:41 -0800

    contact_me/views.py, contact.html: contact-me path returns contact_me app

--- contact_me/views.py
+++ contact_me/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.views import _get_current_year, _read_md_file
 
 # Create your views here.
 def contact_me(request):
     #return HttpResponse('<h1>Contact Me</h1>')
-    context = {}
-    return render(request, 'home/base.html', context)
+    copyright_year = _get_current_year()
+    meta_data = {}
+    content, meta_data = _read_md_file('contact_me/content/contact.md')
+    context = { 
+                'copyright_year': copyright_year,
+                'title': meta_data['title'][0],
+              }
+    return render(request, 'contact_me/contact.html', context)
