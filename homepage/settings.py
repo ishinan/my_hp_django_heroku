e40517c43425642d61393bb593249c3a8309621b
Masaaki Yana <nativeyana@gmail.com>
2019-11-27 16:56:41 -0800

    Changed STATIC_ROOT which required for Heroku but breaks STATICFILES_DIRS

--- homepage/settings.py
+++ homepage/settings.py
@@ -150,14 +150,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "homepage", "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "homepage", "static"),
+]
 
 # Custom NAV_LIST
 
