97d98c34b3891972bef79c97908e20d5ae9d0422
Masaaki Yana <nativeyana@gmail.com>
2019-11-27 16:42:43 -0800

    settings.py: Set STATIC_ROOT

--- homepage/context_processor.py
+++ homepage/context_processor.py
@@ -3,7 +3,6 @@ from homepage import settings
 # The context processor function
 def get_nav_links(request):
     nav_links = settings.DATA_NAV_LIST
-    print(f"NAV: {nav_links}")
     return {
         'navlinks': nav_links,
     }
