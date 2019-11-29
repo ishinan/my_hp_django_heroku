126e3442f7d2a11a767b9f745c0610fd2ea7128a
Masaaki Yana <nativeyana@gmail.com>
2019-11-29 15:56:20 -0800

    favicon.ico: added urlpattern and image for favicon

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
+from django.http import HttpResponseRedirect
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from home import views
+from homepage import settings
+
+
 
 app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    #path('/favicon', views.favicon, name='favicon'),
+    # path('favicon.ico', lambda x: HttpResponseRedirect(settings.STATIC_URL+'favicon.ico'), name='favicon'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=False), name="favicon")
+    #google chrome favicon fix
 ]
