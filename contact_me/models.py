a6ccc3c29f8c79795c8ff3a6b4778eeb9ec4fcb1
Masaaki Yana <nativeyana@gmail.com>
2019-11-29 16:16:14 -0800

    contact_me/models.py: added created_datetime field

--- contact_me/models.py
+++ contact_me/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class ContactMeMessage(models.Model):
         name = models.CharField(max_length=100)
         email = models.EmailField(max_length=120)
         message = models.TextField()
+        created_datetime = models.DateTimeField(default=timezone.now)
